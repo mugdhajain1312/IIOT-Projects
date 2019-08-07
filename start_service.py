0cb11fcd95dde3bc72bf21107df831dba9d16204
mugdhajain1312 <37598682+mugdhajain1312@users.noreply.github.com>
2019-08-07 00:00:34 -0700

    Update start_service.py

--- start_service.py
+++ start_service.py
@@ -5,7 +5,6 @@ import os
 os.system('sudo pm2 stop all')
 os.system('sudo /greengrass/ggc/core/greengrassd start')
 os.system('node get-gg-server-info.js')
-os.chdir('qualcomm-iiot-agriculture')
 os.system('pm2 start app.js')
 os.chdir('../temperature')
 os.system('sudo pm2 start temp-device.py --interpreter="python"')
